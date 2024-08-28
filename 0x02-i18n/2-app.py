e50423cdefdf5daac44cde0e1f98577ae05f5564
Kipchumba Victor <victorkipchumba039@gmail.com>
2024-08-28 05:29:27 +0300

    Task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -17,6 +17,10 @@ app.config.from_object(Config)
 
 babel = Babel(app)
 
+@babel.localeselector
+def get_locale():
+    '''Gets locale'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 @app.route('/')
 def index():
