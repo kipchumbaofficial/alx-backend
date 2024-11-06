e7464d843488d29bd2b6895fde7ed2af386439c8
victor <victorkipchumba039@gmail.com>
2024-11-06 05:07:32 +0300

    Task 0: Correction

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,11 +4,15 @@
 from flask import Flask, render_template
 
 
-app = Flask()
+app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     """ Renders index.html
     """
-    return render_template('index.html')
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
